d80342f7b8667eea8528c8442d4b50d35a5472dc
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-11-02 13:35:37 +0700

    Create Db seeder | Packages

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // daftarkan UserSeeder.php
-        $this->call(UserSeeder::class);
+        $this->call(
+            UserSeeder::class, 
+            PackageSeeder::class
+        );
+
     }
 }
