10594b7872cddea297350cf369f7e460d44d957c
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-11-24 10:34:00 +0700

    Control Transaction

--- app/Http/Controllers/Admin/TransactionController.php
+++ app/Http/Controllers/Admin/TransactionController.php
@@ -4,10 +4,14 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Transaction;
+// use App\Models\Role;
 
 class TransactionController extends Controller
 {
     public function index() {
+        $transactions = Transaction::get();
+        dd($transactions);
         return view('admin.transactions');
     }
 }
