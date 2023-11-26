46006ce368c2f085d613646309b1e1251c6e9b38
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-11-26 17:01:32 +0700

    auth member

---
+++ app/Http/Controllers/Member/LoginController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Member;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    public function index() {
+        return view('member.login');
+    }
+
+    // function login
+    public function auth() {
+
+    }
+
+    // function logout
+    public function logout() {
+        
+    }
+}
