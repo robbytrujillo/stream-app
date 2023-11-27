4ba6a8d4813af68df47db88068c2f256f83a7291
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-11-28 04:13:25 +0700

    Member Dashboard

--- resources/views/member/layouts/base.blade.php
+++ resources/views/member/layouts/base.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Stream</title>
+    <title>Stream | @yield('title')</title>
 
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css" />
@@ -13,7 +13,7 @@
         @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap");
     </style>
 
-    <script src="assets/script/tailwind-config.js"></script>
+    <script src="{{ asset('stream/assets/script/tailwind-config.js') }}"></script>
 
     <style type="text/tailwindcss">
         @layer components{
@@ -39,6 +39,7 @@
                 {{-- navbar --}}
                 @include('member.layouts.navbar') 
 
+                @yield('content')
             </div>
         </div>
         <!-- END: Content -->
@@ -53,7 +54,7 @@
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"
         integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-    <script src="assets/script/script.js"></script>
+    <script src="{{ asset('stream/assets/script/script.js') }}"></script>
     <script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script>
     <script>
         $(document).ready(function () {
