55669f13a2dd296e19bb4bd50a40a94778415465
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-11-28 19:17:46 +0700

    Member Dashboard 2

--- app/Http/Controllers/Member/DashboardController.php
+++ app/Http/Controllers/Member/DashboardController.php
@@ -4,10 +4,16 @@ namespace App\Http\Controllers\Member;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Movie; // tambahkan models movie
 
 class DashboardController extends Controller
 {
     public function index() {
+        // panggil data movie
+        $movies = Movie::orderBy('featured', 'DESC')
+                  ->orderBy('created_at', 'DESC')
+                  ->get();
+
         return view('member.dashboard');
     }
 }
