ccd84608b73ab7efb88992a4a1e2008966a7daa1
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-12-04 06:12:24 +0700

    Member Dashboard | pricing - midtrans

--- app/Http/Controllers/Member/TransactionController.php
+++ app/Http/Controllers/Member/TransactionController.php
@@ -11,7 +11,53 @@ use App\Illuminate\Support\Str;
 class TransactionController extends Controller
 {
     public function store(Request $request) {
-        dd($request->all());
-        return "test";
+        // dd($request->all());
+        // return "test";
+
+
+
+
+        /*Install Midtrans PHP Library (https://github.com/Midtrans/midtrans-php)
+composer require midtrans/midtrans-php
+                              
+Alternatively, if you are not using **Composer**, you can download midtrans-php library 
+(https://github.com/Midtrans/midtrans-php/archive/master.zip), and then require 
+the file manually.   
+
+require_once dirname(__FILE__) . '/pathofproject/Midtrans.php'; */
+
+$package = Package::find($request->package_id);
+
+$transaction = Transaction::create() {[
+    'package_id' => $package->id,
+]
+
+}
+
+//SAMPLE REQUEST START HERE
+
+// Set your Merchant Server Key
+\Midtrans\Config::$serverKey = 'YOUR_SERVER_KEY';
+// Set to Development/Sandbox Environment (default). Set to true for Production Environment (accept real transaction).
+\Midtrans\Config::$isProduction = false;
+// Set sanitization on (default)
+\Midtrans\Config::$isSanitized = true;
+// Set 3DS transaction for credit card to true
+\Midtrans\Config::$is3ds = true;
+
+$params = array(
+    'transaction_details' => array(
+        'order_id' => rand(),
+        'gross_amount' => 10000,
+    ),
+    'customer_details' => array(
+        'first_name' => 'budi',
+        'last_name' => 'pratama',
+        'email' => 'budi.pra@example.com',
+        'phone' => '08111222333',
+    ),
+);
+
+$snapToken = \Midtrans\Snap::getSnapToken($params);
     }
 }
