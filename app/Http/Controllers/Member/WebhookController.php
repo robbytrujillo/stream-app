6dbb185225276097a8abdd4142792f81e99293a2
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-12-07 05:59:48 +0700

    Member Dashboard | handler

--- app/Http/Controllers/Member/WebhookController.php
+++ app/Http/Controllers/Member/WebhookController.php
@@ -8,9 +8,38 @@ use Illuminate\Http\Request;
 class WebhookController extends Controller
 {
     public function handler(Request $request) {
-        require_once(dirname(__FILE__) . '/Midtrans.php');
-        \Midtrans\Config::$isProduction = false;
-        \Midtrans\Config::$serverKey = '<your serverkey>';
+        //require_once(dirname(__FILE__) . '/Midtrans.php');
+        \Midtrans\Config::$isProduction = (bool) env('MIDTRANS_IS_PRODUCTION');
+        \Midtrans\Config::$serverKey = env('MIDTRANS_SERVER_KEY');
         $notif = new \Midtrans\Notification();
+        
+        $transactionStatus = $notif->transaction_status;
+        $transactionCode = $notif->order_id;
+        $fraudStatus = $notif->fraud_status;
+
+        $status = '';
+
+        // take the settings from nodejs
+        if ($transactionStatus == 'capture'){
+            if ($fraudStatus == 'chalange'){
+                    // TODO set transaction status on your database to 'success'
+                    // and response with 200 OK
+                    $status = 'chalange';
+                } else if ($fraudStatus == 'accept'){
+
+                }
+            } else if ($transactionStatus == 'settlement'){
+                // TODO set transaction status on your database to 'success'
+                // and response with 200 OK
+            } else if ($transactionStatus == 'cancel' ||
+              $transactionStatus == 'deny' ||
+              $transactionStatus == 'expire'){
+              // TODO set transaction status on your database to 'failure'
+              // and response with 200 OK
+            } else if ($transactionStatus == 'pending'){
+              // TODO set transaction status on your database to 'pending' / waiting payment
+              // and response with 200 OK
+            }
+
     }
 }
