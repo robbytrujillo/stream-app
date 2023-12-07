d1ab22d1a81411c130ecc0224edfcae0d17e452c
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-12-08 05:31:06 +0700

    member | webhook controller

--- app/Http/Controllers/Member/WebhookController.php
+++ app/Http/Controllers/Member/WebhookController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers\Member;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Transaction;
+use App\Models\Package;
+use App\Models\UserPremium;
+
 
 class WebhookController extends Controller
 {
@@ -22,24 +26,32 @@ class WebhookController extends Controller
         // take the settings from nodejs
         if ($transactionStatus == 'capture'){
             if ($fraudStatus == 'chalange'){
-                    // TODO set transaction status on your database to 'success'
-                    // and response with 200 OK
                     $status = 'chalange';
                 } else if ($fraudStatus == 'accept'){
-
+                    $status = 'success';
                 }
             } else if ($transactionStatus == 'settlement'){
-                // TODO set transaction status on your database to 'success'
-                // and response with 200 OK
+                $status = 'success';
             } else if ($transactionStatus == 'cancel' ||
               $transactionStatus == 'deny' ||
               $transactionStatus == 'expire'){
-              // TODO set transaction status on your database to 'failure'
-              // and response with 200 OK
+              $status = 'failure';
             } else if ($transactionStatus == 'pending'){
-              // TODO set transaction status on your database to 'pending' / waiting payment
-              // and response with 200 OK
+              $status = 'pending';
             }
 
+            $transaction = Transaction::with('package')
+              ->where('transaction_code', $transactionCode)
+              ->first();
+
+              if ($status === 'success') {
+                UserPremium::create([
+                    'package_id' => $transaction->package->id,
+                    'user_id' => $transaction->user->id,
+                    'end_of_subscription' =>now()->addDays($transaction->package->max_days)
+                ]);
+              }
+
+              $transaction->update(['status' => $status]);
     }
 }
