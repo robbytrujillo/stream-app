e3aee7cb504f2f1c8f127972d02e252b4dde63c1
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-12-12 23:29:26 +0700

    member Watching

--- app/Http/Controllers/Member/MovieController.php
+++ app/Http/Controllers/Member/MovieController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Member;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Movie;
+use App\Models\UserPremium;
 
 class MovieController extends Controller
 {
@@ -13,5 +14,12 @@ class MovieController extends Controller
         
         // lempar data ke view
         return view('member.movie-detail', ['movie' => $movie]); // parsing data
+    }
+
+    //12122023
+    public function watch($id) {
+        $userId = auth()->user()->id;
+
+        $userPremium = USerPremium::where('user_id', $userId)->first();
     }
 }
